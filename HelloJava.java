ed279f9792e0b77c2d65b75a0010b96f4148dc1d
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2023-10-15 18:44:32 -0300

    Add files via upload

---
+++ HelloJava.java
@@ -0,0 +1,19 @@
+class newJavaFile
+{
+public static void main (String entrada[])
+	{
+int inteiro = 40;
+char caracter = 'F';
+double real = 1.76;
+String frase = "Carlos";
+boolean VF = true;
+
+	if (VF == true)
+	{
+	 System.out.println("Hello World, sou" + frase + "tenho " + inteiro +
+	"anos e tenho " + real + "m de altura");
+	}
+
+	System.exit(0);
+ }
+}
